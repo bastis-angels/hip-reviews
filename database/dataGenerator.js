e053fff7a25ee17f501d52617343f22859fc2443
basti <bastipacudan@gmail.com>
2019-03-29 20:22:14 -0700

    fix infinite loop on dataGenerator

--- database/dataGenerator.js
+++ database/dataGenerator.js
@@ -3,14 +3,14 @@ const faker = require('faker');
 const sequelize = require('./sequelize.js');
 
 // // generate locations data
-for (let i = 1; i < 5; i += 1) {
+for (let i = 1; i < 100; i += 1) {
 // save each location to locations table in user_reviews database
   sequelize.saveLocation({
     loc_id: i,
     loc_name: faker.address.city(),
   });
   // generate random number of reviews and user for each location
-  for (let x = 1; x < Math.floor(Math.random() * Math.floor(5)); x += 1) {
+  for (let x = 1; x < Math.floor(Math.random() * Math.floor(25)); x += 1) {
     // save each user to users table in user_reviews database
     sequelize.saveUser({
       name: faker.name.findName(),
@@ -21,7 +21,7 @@ for (let i = 1; i < 5; i += 1) {
       user_id: x,
       loc_id: i,
       context: faker.lorem.sentence(),
-      helpful: Math.floor(Math.random() * Math.floor(5)),
+      helpful: Math.floor(Math.random() * Math.floor(10)),
     });
     // generate random number of images for each review
     for (let y = 1; y < Math.floor(Math.random() * Math.floor(5)); y += 1) {
