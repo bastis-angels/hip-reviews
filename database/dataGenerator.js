67746e9ce2aa002ce13f2d53d27e2d203b79f124
basti <bastipacudan@gmail.com>
2019-03-30 20:07:26 -0700

    added images url from amazon s3

--- database/dataGenerator.js
+++ database/dataGenerator.js
@@ -12,9 +12,10 @@ for (let i = 1; i < 100; i += 1) {
   // generate random number of reviews and user for each location
   for (let x = 1; x < Math.floor(Math.random() * Math.floor(15)); x += 1) {
     // save each user to users table in user_reviews database
+    const avatarId = Math.floor(Math.random() * 9) + 1;
     sequelize.saveUser({
       name: faker.name.findName(),
-      avatar: '',
+      avatar: `https://s3.us-east-2.amazonaws.com/bastis-camp-reviews-photos/avatars/${avatarId}.png`,
     });
     // save each review to reviews table in user_reviews database
     sequelize.saveReview({
@@ -26,10 +27,11 @@ for (let i = 1; i < 100; i += 1) {
     // generate random number of images for each review
     for (let y = 1; y < Math.floor(Math.random() * Math.floor(5)); y += 1) {
     // save each image for each review in images table in user_reviews database
+      const imgId = Math.floor(Math.random() * 10) + 1;
       sequelize.saveImage({
         review_id: x,
         image_description: faker.lorem.sentences(),
-        image_url: '',
+        image_url: `https://s3.us-east-2.amazonaws.com/bastis-camp-reviews-photos/review-img/${imgId}.jpg`,
       });
     }
   }
